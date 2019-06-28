77233a744ee66602c4304149c21aa87ebead3865
John Lim (Win Desktop) <limjohn@uw.edu>
2019-06-28 15:59:41 -0700

    .

--- index.js
+++ index.js
@@ -1,10 +1,62 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const io = require('socket.io');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+
+let ENusers = [];
+let KOusers = [];
+
+let server = app.use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/en/', (req, res) => res.render('pages/index', { REGION : "en" }))
+    .get('/ko/', (req, res) => res.render('pages/index', { REGION : "ko" }))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+let sio = io.listen(server);
+
+const io_english = sio.of('/en');
+const io_korean = sio.of('/ko');
+
+io_english.on('connection', function(socket){
+
+    // Increase user number
+    ENusers.push(socket.id);
+
+    // Init people & broadcast entering.
+    socket.emit('init', ENusers);
+    socket.broadcast.emit('enter', socket.id);
+    console.log('[EN] user '+ socket.id  +' connected');
+
+    // On Disconnect
+    socket.on('disconnect', function(){
+        console.log('[EN] user ' +  socket.id + ' disconnected');
+        ENusers = removeFromArray(ENusers, socket.id);
+        socket.broadcast.emit('exit', socket.id);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function removeFromArray(arr, el) {
+    let index = arr.indexOf(el);
+    if (index > -1) {
+        arr.splice(index, 1);
+    }
+    return arr;
+}
