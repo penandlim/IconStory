e2dc31220d2e2a9ce8ca91bb65338d13544f9a28
John Lim (Win Desktop) <limjohn@uw.edu>
2019-07-30 09:32:12 -0700

    leggo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const io = require('socket.io');
 const path = require('path');
+const request = require('request');
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -8,17 +10,41 @@ const app = express();
 let ENusers = [];
 let KOusers = [];
 
+let EditingUsers = new Array(16).fill(0);
+
+let URL = "https://bicon.net.solidwallet.io/api/v3"; // TESTNET
+let SCORE = "cx3d574c55a86f0d98a5a24424fcc604902b81dcc4"; // TESTNET
+
 let server = app.use(express.static(path.join(__dirname, 'public')))
+    .use(bodyParser.json()) // support json encoded bodies
+    .use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
-    .get('/en/', (req, res) => res.render('pages/index', { REGION : "en" }))
-    .get('/ko/', (req, res) => res.render('pages/index', { REGION : "ko" }))
+    .get('/', (req, res) => res.render('pages/main', {URL: URL}))
+    .get('/en/', (req, res) => res.render('pages/index', { REGION : "en" , URL: URL}))
+//    .get('/ko/', (req, res) => res.render('pages/index', { REGION : "ko" , URL: URL}))
+    .post('/api/', function(req, res) {
+
+        request.post({
+            url:     URL,
+            body:    req.body,
+            json: true
+        }, function(error, response, body){
+            if (body) {
+                console.log(body);
+                res.send(JSON.stringify(body));
+            }
+            else
+                res.send(error);
+        });
+
+    })
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 let sio = io.listen(server);
 
 const io_english = sio.of('/en');
-const io_korean = sio.of('/ko');
+// const io_korean = sio.of('/ko');
 
 io_english.on('connection', function(socket){
 
@@ -26,7 +52,7 @@ io_english.on('connection', function(socket){
     ENusers.push(socket.id);
 
     // Init people & broadcast entering.
-    socket.emit('init', ENusers);
+    socket.emit('init', ENusers, EditingUsers);
     socket.broadcast.emit('enter', socket.id);
     console.log('[EN] user '+ socket.id  +' connected');
 
@@ -34,18 +60,33 @@ io_english.on('connection', function(socket){
     socket.on('disconnect', function(){
         console.log('[EN] user ' +  socket.id + ' disconnected');
         ENusers = removeFromArray(ENusers, socket.id);
-        socket.broadcast.emit('exit', socket.id);
+
+        let i = EditingUsers.indexOf(socket.id);
+        if (i > -1) {
+            EditingUsers[i] = 0;
+        }
+
+        socket.broadcast.emit('exit', socket.id, i);
     });
+
+    socket.on('startWriting', function(index){
+        if (index < EditingUsers.length) {
+            EditingUsers[index] = socket.id;
+            socket.broadcast.emit('startWriting', socket.id, index);
+        }
+    });
+
+    socket.on('cancelWriting', function(index){
+        socket.broadcast.emit('cancelWriting', socket.id, index);
+        EditingUsers = removeAndReplaceArrayEl(EditingUsers, socket.id, 0);
+    });
+
+    socket.on('finishWriting', function(index, isInsert){
+        socket.broadcast.emit('finishWriting', socket.id, index, isInsert);
+        EditingUsers = removeAndReplaceArrayEl(EditingUsers, socket.id, 0);
+    });
+
 });
-
-
-
-
-
-
-
-
-
 
 
 
@@ -57,6 +98,14 @@ function removeFromArray(arr, el) {
     let index = arr.indexOf(el);
     if (index > -1) {
         arr.splice(index, 1);
+    }
+    return arr;
+}
+
+function removeAndReplaceArrayEl(arr, el, replaceTo) {
+    let index = arr.indexOf(el);
+    if (index > -1) {
+        arr[index] = replaceTo;
     }
     return arr;
 }
---
+++ public/js/helper.js
@@ -0,0 +1,60 @@
+var mb32=s=>t=>(s=s+1831565813|0,t=Math.imul(s^s>>>15,1|s),t=t+Math.imul(t^t>>>7,61|t)^t,(t^t>>>14)>>>0)/2**32;
+// Better 32-bit integer hash function: https://burtleburtle.net/bob/hash/integer.html
+var hash=n=>(n=61^n^n>>>16,n+=n<<3,n=Math.imul(n,668265261),n^=n>>>15)>>>0;
+
+const COLORS = ["#4F6CA6", "#EFA21E", "#DC495C", "#5B8545", "#050505", "#FFFFFF"];
+
+function calculateStyle(s) {
+
+    let code = 0;
+    for (let i = 0; i < s.length; i++) {
+        code += s.charCodeAt(i);
+    }
+
+    let i = Math.floor(mb32(hash(code))() * 5.99999);
+    let j = i;
+    for (let k = 0; i === j; k++) {
+        j = Math.floor(mb32(hash(code + s.length * k))() * 5.99999);
+    }
+    return [i, j]
+}
+
+function randomSize(s) {
+    let code = 0;
+    for (let i = 0; i < s.length; i++) {
+        code -= s.charCodeAt(i);
+    }
+
+    let i = mb32(hash(code))();
+    return 1 + i;
+}
+
+function escapeHtml(text) {
+    var map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
+    };
+
+    return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
+
+function placeCaretAtEnd(el) {
+    el.focus();
+    if (typeof window.getSelection != "undefined"
+        && typeof document.createRange != "undefined") {
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        range.collapse(false);
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (typeof document.body.createTextRange != "undefined") {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(el);
+        textRange.collapse(false);
+        textRange.select();
+    }
+}
